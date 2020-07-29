b95fbea94c581a29646e91df2f68e115adb60d34
Kol_ko <kolianos007@gmai.com>
2020-07-29 20:53:23 +0300

    add method fetch and axios

--- js/script.js
+++ js/script.js
@@ -1,46 +1,72 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   function req() {
-    const request = new XMLHttpRequest();
-    request.open("GET", "http://localhost:3000/people");
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
-    request.addEventListener('load', function() {
-      if (request.readyState === 4 && request.status == 200) {
-        let data = JSON.parse(request.response);
-        console.log(data)
+    // const request = new XMLHttpRequest();
+    // request.open("GET", "http://localhost:3000/people");
+    // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    // request.send();
+    // request.addEventListener('load', function() {
+    //   if (request.readyState === 4 && request.status == 200) {
+    //     let data = JSON.parse(request.response);
+    //     console.log(data)
+    //     createCards(data)
 
-        data.forEach(item => {
-          let card = document.createElement('div')
+    //   } else {
+    //     console.error('Что-то пошло не так')
+    //   }
+    // });
 
-          card.classList.add('card')
-
-          let icon;
-          if (item.sex === 'male') {
-            icon = 'icons/mars.png'
-          } else {
-            icon = 'icons/female.png'
-          }
-
-          card.innerHTML = `
-            <img src="${item.photo}" alt="photo">
-            <div class="name">${item.name} ${item.surname}</div>
-            <div class="sec">
-                <img src="${icon}" alt="male">
-            </div>
-            <div class="age">${item.age}</div>
-          `
-          document.querySelector('.app').appendChild(card)
-        })
-      } else {
-        console.error('Что-то пошло не так')
-      }
-    });
+    getResource("http://localhost:3000/people")
+      .then(data => createCards(data.data))
+      .catch(err => console.log(err))
 
     this.remove()
   }
 
   document.querySelector('button').addEventListener('click', req, {'once': true})
 
+  // async function getResource(url) {
+  //   const res = await fetch(`${url}`)
 
+  //   if (!res.ok) {
+  //     throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+  //   }
+
+  //   return await res.json()
+  // }
+
+  async function getResource(url) {
+    const res = await axios(`${url}`)
+
+    if (res.status !== 200) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+    }
+
+    return res;
+  }
+
+  function createCards(response) {
+    response.forEach(item => {
+      let card = document.createElement('div')
+
+      card.classList.add('card')
+
+      let icon;
+      if (item.sex === 'male') {
+        icon = 'icons/mars.png'
+      } else {
+        icon = 'icons/female.png'
+      }
+
+      card.innerHTML = `
+        <img src="${item.photo}" alt="photo">
+        <div class="name">${item.name} ${item.surname}</div>
+        <div class="sec">
+            <img src="${icon}" alt="male">
+        </div>
+        <div class="age">${item.age}</div>
+      `
+      document.querySelector('.app').appendChild(card)
+    })
+  }
 })
